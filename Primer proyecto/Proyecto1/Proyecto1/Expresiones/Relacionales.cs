08fa72d1f2da6ddad290b3314b744e04deaaaec8
Ronald Romero <3606169920101@ingenieria.usac.edu.gt>
2021-02-15 10:17:12 -0600

    Se creó la clase para el manejo de relacionales

---
+++ Primer proyecto/Proyecto1/Proyecto1/Expresiones/Relacionales.cs
@@ -0,0 +1,19 @@
+﻿using Proyecto1.Administracion;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proyecto1.Expresiones
+{
+    public class Relacionales : NodoAST
+    {
+        public Relacionales() : base(0, 0)
+        {
+
+        }
+        public override Objeto ejecutar()
+        {
+            return null;
+        }
+    }
+}
